c4c882860a94cf778bddcbc97c57a2fe500a86fe
SasiDharKM <sasiboss.rock@gmail.com>
2016-12-14 19:31:12 +0530

    changed token.mat for compatiblity-issue resolution

--- Week2/Assignment/gradientDescent.m
+++ Week2/Assignment/gradientDescent.m
@@ -16,17 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-delta = (1/m)*sum(X.*repmat((X*theta - y),1,size(X,2)));
-
-theta = (theta' - (alpha*delta))';
-
-
-
-
-
+delta = (1/m)*sum(X.*repmat((X*theta - y), 1, size(X,2))); 
+    theta = (theta' - (alpha * delta))';
     % ============================================================
-
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
